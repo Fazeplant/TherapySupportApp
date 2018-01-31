bf4424f8f00fa84a8d4979fbf1aecb7f43e61139
Fazeplant <bumes.michael@googlemail.com>
2018-01-31 18:17:53 +0100

    RecyclerView zum Anzeigen der gespeicherten Medizin hinzugefügt.

---
+++ app/src/main/java/michaelbumes/therapysupportapp/adapter/DrugAdapter.java
@@ -0,0 +1,54 @@
+package michaelbumes.therapysupportapp.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import michaelbumes.therapysupportapp.R;
+import michaelbumes.therapysupportapp.entity.Drug;
+
+/**
+ * Created by Michi on 31.01.2018.
+ */
+
+public class DrugAdapter extends RecyclerView.Adapter<DrugAdapter.ViewHolder>{
+   List<Drug> drugs;
+
+    public DrugAdapter(List<Drug> drugs) {
+        this.drugs = drugs;
+    }
+
+    @Override
+    public DrugAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.drugs_row, parent,false );
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(DrugAdapter.ViewHolder holder, int position) {
+        holder.drugName.setText(drugs.get(position).getDrugName());
+        holder.drugManufacturer.setText(drugs.get(position).getManufacturer());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return drugs.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView drugName;
+        public TextView drugManufacturer;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            drugName = itemView.findViewById(R.id.drug_name);
+            drugManufacturer = itemView.findViewById(R.id.drug_manufacturer);
+        }
+    }
+}
--- app/src/main/java/michaelbumes/therapysupportapp/fragments/DrugPlanFragment.java
+++ app/src/main/java/michaelbumes/therapysupportapp/fragments/DrugPlanFragment.java
@@ -6,19 +6,28 @@ import android.app.Fragment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.ncapdevi.fragnav.FragNavController;
+import java.util.ArrayList;
+import java.util.List;
 
 import michaelbumes.therapysupportapp.R;
+import michaelbumes.therapysupportapp.adapter.DrugAdapter;
+import michaelbumes.therapysupportapp.database.AppDatabase;
+import michaelbumes.therapysupportapp.entity.Drug;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DrugPlanFragment extends BaseFragment {
     FloatingActionButton fab;
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+
 
 
     public static DrugPlanFragment  newInstance(int instance) {
@@ -35,11 +44,22 @@ public class DrugPlanFragment extends BaseFragment {
         getActivity().setTitle(R.string.title_drug_plan);
 
 
+
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_drug_plan, container, false);
+
+        recyclerView = view.findViewById(R.id.recyler_view);
+
+        List<Drug> drugs = AppDatabase.getAppDatabase(getContext()).drugDao().getAll();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new DrugAdapter(drugs);
+        recyclerView.setAdapter(adapter);
+
+
 
 
         fab = (FloatingActionButton)view.findViewById(R.id.fab);
